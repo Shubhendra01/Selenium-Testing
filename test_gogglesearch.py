68e8898b31889ae0e8ed3b1b16cacaf19deefd2c
unknown <shubhhhh17@gmail.com>
2022-03-30 15:26:20 +0530

    Selenium testing google chrome done successfully

--- test_gogglesearch.py
+++ test_gogglesearch.py
@@ -1,10 +1,16 @@
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+import time
 
-driver = webdriver.Chrome(ChromeDriverManager().instal())
+driver = webdriver.Chrome(ChromeDriverManager().install())
 print("Test case started")
 driver.maximize_window()
 driver.get("https://www.google.com/")
+time.sleep(1)
+driver.find_element_by_name("q").send_keys("Harman")
+time.sleep(1)
+driver.find_element_by_name("btnK").click()
+
 time.sleep(5)
 driver.close()
 print("Test case has successfully completed")
