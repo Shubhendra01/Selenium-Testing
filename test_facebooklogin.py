041049b5cddd82ff3642eb33b0bd1a8ad6619c3a
unknown <shubhhhh17@gmail.com>
2022-03-31 16:12:41 +0530

    Selenium testing of facebook using pytest cases done successfully

--- test_facebooklogin.py
+++ test_facebooklogin.py
@@ -1,16 +1,22 @@
+import pytest
+import time
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
-import time
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
-print("Test case started")
-driver.maximize_window()
-driver.get("https://www.facebook.com/")
-time.sleep(1)
-driver.find_element_by_name("email").send_keys("Shubhendra Singh")
-driver.find_element_by_name("pass").send_keys("454545")
-time.sleep(1)
-driver.find_element_by_name("login").click()
-time.sleep(5)
-driver.close()
-print("Test is successfully conducted")
+@pytest.fixture()
+def setUp():
+    global driver
+    driver = webdriver.Chrome(ChromeDriverManager().install())
+    driver.maximize_window()
+    yield
+    time.sleep(5)
+    driver.close()
+
+def test_facebooklogin(setUp):
+    driver.get("https://www.facebook.com/")
+    time.sleep(1)
+    driver.find_element_by_name("email").send_keys("Shubhendra Singh")
+    driver.find_element_by_name("pass").send_keys("454545")
+    time.sleep(1)
+    driver.find_element_by_name("login").click()
+    time.sleep(5)
